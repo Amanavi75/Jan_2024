f79b2183e185fa064b012fae6136c4481deb2d1d
Suman Sourabh <sumansourabh9155@gmail.com>
2025-03-27 09:45:27 +0530

    asdsdsdsdsd

--- src/components/Landingsection/AllProject/CaseStudy.jsx
+++ src/components/Landingsection/AllProject/CaseStudy.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CarterDSL from "../../../assets/Project/CarterDSL.png";
-import CarterRedesign from "../../../assets/Project/CartetRedesign.png";
+import CarterRedesign from "../../../assets/Project/CarterRedesign.png";
 import UVC from "../../../assets/Project/uvc.png";
 import Raikar from "../../../assets/Project/raikar.webp";
 
